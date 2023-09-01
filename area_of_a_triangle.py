8dadcf5896c667438945adb435b554b5750dd431
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 21:32:09 +0530

    area_of_triangle

---
+++ area_of_a_triangle.py
@@ -0,0 +1,11 @@
+#Q3. Create a program that calculates and displays the area of a triangle using the base and height provided by the user.
+
+#input section
+a=int(input("enter the base"))
+b=int(input("enter the height"))
+
+#logic section
+A=(0.5)*a*b
+
+#display section
+print("the area of triangle is",A)
