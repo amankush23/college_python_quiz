05bab826d0fe767f9e69c7099297978592a843b4
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 21:42:14 +0530

    sum_of_all_even_numbers.py

---
+++ sum_of_all_even_numbers.py
@@ -0,0 +1,13 @@
+#Q7. Write a program that calculates and displays the sum of all even numbers from 1 to a user-
+
+
+#input section
+
+a=int(input("enter the number:"))
+
+#logic section
+n=a/2
+out=n*(n+1)
+
+     #display section
+print(out)
