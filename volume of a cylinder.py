2483e6a7beba703c7fc96df4114630357e3ac427
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 21:43:10 +0530

    volume of a cylinder.py

---
+++ volume of a cylinder.py
@@ -0,0 +1,13 @@
+#Q10. Develop a program that calculates the volume of a cylinder. Ask the user for the radius and height
+
+#input section
+a=int(input("enter the height of cylinder"))
+b=int(input("enter the radius of cylinder"))
+
+
+#logic section
+V=3.14*b*b*a
+
+#display section
+
+print("the volume of cylinder",V )
