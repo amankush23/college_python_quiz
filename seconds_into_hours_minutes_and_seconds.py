6d2c3641f8e0e86389131345cdede9b189502d7a
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 21:41:32 +0530

    seconds_into_hours_minutes_and_seconds.py

---
+++ seconds_into_hours_minutes_and_seconds.py
@@ -0,0 +1,12 @@
+#Q6. Create a program that converts a given number of seconds into hours, minutes, and seconds.
+
+#input section
+a=int(input("seconds:"))
+
+#logic section
+h=a//3600
+m=a//60
+s=()
+
+#display section
+print("hours are",h,"minutes are",m,"and seconds are",s)
