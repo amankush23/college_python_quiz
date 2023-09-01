cb0f084b507733d8eb91e09fee456bef05a8ae4c
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 21:38:26 +0530

    greatest_number

---
+++ Greatest_of_Two_Numbers.py
@@ -0,0 +1,11 @@
+# Q5. Find the Greatest of Two Numbers in Python.
+
+#input section
+a=int(input("enter the first value:"))
+b=int(input("enter the second value:"))
+
+#logic section
+result=max(a,b)
+
+#display section
+print("the greater number is",result)
