2e58bb6cb090f6140ab3d0fe7637e445e7f719b4
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 21:40:01 +0530

    Numbers_convert_into_weeks_and_days

---
+++ Numbers_convert_into_weeks_and_days.py
@@ -0,0 +1,15 @@
+#write a program that converts a given number of days into weeks and days.
+
+#input section
+
+a=int(input("enter the number of days: "))
+
+#logic section
+out1=a//7
+out2=a%7
+
+#display section
+print("the weeks are",out1,"and the days are",out2)
+
+
+
